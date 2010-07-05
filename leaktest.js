08f40866c7200ae52ef31f509f615fabc21c2c11
Astro <astro@spaceboyz.net>
2010-07-05 03:15:15 +0200

    leaktest.js: pretty memory stats

--- leaktest.js
+++ leaktest.js
@@ -1,10 +1,23 @@
 var sys = require('sys');
 var expat = require('./build/default/expat');
 
+var i = 0;
+
 function foo() {
     var p = new expat.Parser();
     process.nextTick(foo);
-    console.log(sys.inspect(process.memoryUsage()));
+    i++;
 }
 
 foo();
+
+setInterval(function() {
+		var mem = process.memoryUsage();
+		sys.print(i+" Iterations: ");
+		for(var k in mem) {
+		    sys.print("\t"+k+"="+mem[k]);
+		}
+		sys.print("\n");
+
+		i = 0;
+	    }, 100);
